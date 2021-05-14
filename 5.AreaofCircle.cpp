cfa564baf538c5ba39a03eed3e3dad5773156e62
AyeshaSahar <83140034+AyeshaSahar@users.noreply.github.com>
2021-05-15 00:49:36 +0500

    Add files via upload

---
+++ 5.AreaofCircle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int pi,r,area;
+	cout<<"r(in cm)=";
+	cin>>r;
+	pi = 3.142;
+	area = pi*r*r;
+	cout<<"area of circle="<<area;
+	return 0;
+}
